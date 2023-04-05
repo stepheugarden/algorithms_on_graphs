7edb9fa2eeeab17a76eac73decadfecf1fe6a050
stepheugarden <stepheu@protonmail.com>
2023-04-05 11:46:52 +0200

    added solution to connected components

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 https://www.coursera.org/learn/algorithms-on-graphs
 
-- Week 1 "Graph decomposition": depth first reachablity algorithm implemented
+- Week 1:
+  - *Graph decomposition*: depth first reachablity algorithm implemented
+  - *Connected components*: get number of connected components in a graph
--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -1,21 +1,75 @@
-#Uses python3
+# Uses python3
 
 import sys
 
 
+# Uses python3
+
+import sys
+import os
+import re
+from utilities import read_test_cases
+
+reached = set()
+
+
+def explore(adj, start):
+    reached.add(start)
+
+    for node in adj[start]:
+        if node in reached:
+            continue
+        explore(adj, node)
+
+
 def number_of_components(adj):
     result = 0
-    #write your code here
+    for node in range(len(adj)):
+        if node in reached:
+            continue
+        result += 1
+        explore(adj, node)
+
     return result
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+
+INTERACTIVE = False
+
+
+def create_adjacency_list(data):
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(number_of_components(adj))
+    return adj
+
+
+def parse_input(file_path):
+    data = list(map(int, re.split("\s|\n", open(file_path).read())))
+    return create_adjacency_list(data)
+
+
+if __name__ == "__main__":
+
+    if INTERACTIVE:
+        input = sys.stdin.read()
+        data = list(map(int, input.split()))
+        adj = create_adjacency_list(data)
+        print(number_of_components(adj))
+    else:
+        path, files = read_test_cases.return_path_files(
+            "week1_graph_decomposition1", "1_finding_exit_from_maze"
+        )
+        for file in files:
+            reached = set()
+            reached_finish = False
+            file_path = os.path.join(path, file)
+            adj = parse_input(file_path)
+            res = number_of_components(adj)
+            sol = int(open(file_path + ".a").read())
+
+            assert sol == res, f"Error in {file=}"
+            print(f"connected components in {file}: {res}")
