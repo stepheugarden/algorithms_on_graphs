2ff52a9b078e457168c2f65c22aa54b90a599552
stepheugarden <stepheu@protonmail.com>
2023-04-13 15:06:10 +0200

    solution to topological ordering of DAGs

--- week2_graph_decomposition2/2_order_of_courses/toposort.py
+++ week2_graph_decomposition2/2_order_of_courses/toposort.py
@@ -1,28 +1,102 @@
-#Uses python3
+# Uses python3
 
+import os
+import re
 import sys
+from utilities import read_test_cases
+
 
 def dfs(adj, used, order, x):
-    #write your code here
+    # write your code here
     pass
 
 
-def toposort(adj):
-    used = [0] * len(adj)
-    order = []
-    #write your code here
-    return order
+def parse_input(file_path):
+    data = list(map(int, re.split("\s|\n", open(file_path).read())))
+    return create_adjacency_list(data)
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+
+def create_adjacency_list(data):
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    order = toposort(adj)
-    for x in order:
-        print(x + 1, end=' ')
+    return adj
 
+
+def is_sink(vertex, not_used):
+    return len(not_used) == 0
+
+
+# def create_adjacency_set(adj):
+# adj_set = [set()] * len(adj)
+# for source in range(len(adj)):
+# adj_set[source] = set(adj[source])
+
+# return adj_set
+
+
+def toposort(adj):
+    used = [False] * len(adj)
+    order = []
+    # adj_set = create_adjacency_set(adj)
+
+    def follow_edges(vertex):
+        not_used = [vert for vert in adj[vertex] if not used[vert]]
+        if is_sink(vertex, not_used):
+            used[vertex] = True
+            order.append(vertex)
+            return
+
+        for vert in not_used:
+            if used[vert]:
+                continue
+            follow_edges(vert)
+
+        follow_edges(vertex)
+
+    for vert in range(len(adj)):
+        if used[vert]:
+            continue
+        follow_edges(vert)
+
+    return list(reversed(order))
+
+
+def check_topological_order(adj, order):
+
+    while len(order) > 0:
+        vertex = order.pop()
+        dest = set(adj[vertex])
+        for prev in order:
+            if prev in dest:
+                print(f"{vertex} is reachable from {prev}, i.e. no valid ordering")
+                return False
+    return True
+
+
+INTERACTIVE = False
+
+if __name__ == "__main__":
+
+    if INTERACTIVE:
+        input = sys.stdin.read()
+        data = list(map(int, input.split()))
+        adj = create_adjacency_list(data)
+        res = toposort(adj)
+        for x in res:
+            print(x + 1, end=" ")
+    else:
+        path, files = read_test_cases.return_path_files(
+            "week2_graph_decomposition2", "2_order_of_courses"
+        )
+        for file in files:
+            file_path = os.path.join(path, file)
+            adj = parse_input(file_path)
+            order = toposort(adj)
+            res = " ".join([str(r + 1) for r in order])
+
+            assert check_topological_order(adj, order), f"Error in {file=}"
+            print(f"topological order of {file}: {res}")
