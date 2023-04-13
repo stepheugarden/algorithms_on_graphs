631142f2711cfc24e73dff5031081e7ba43f1f93
stepheugarden <stepheu@protonmail.com>
2023-04-13 11:30:29 +0200

    solution for checking DAG property

--- week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
+++ week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
@@ -1,18 +1,70 @@
-#Uses python3
+# Uses python3
 
+import os
+import re
 import sys
+from utilities import read_test_cases
 
 
-def acyclic(adj):
-    return 0
-
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+def create_adjacency_list(data):
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    print(acyclic(adj))
+    return adj
+
+
+def parse_input(file_path):
+    data = list(map(int, re.split("\s|\n", open(file_path).read())))
+    return create_adjacency_list(data)
+
+
+def acyclic(adj):
+    visited = [False] * len(adj)
+    has_cycle = False
+
+    # recursively follow paths
+    def follow_edges(vertex, has_cycle):
+        if visited[vertex]:
+            has_cycle = True
+            return has_cycle
+        visited[vertex] = True
+
+        for vert in adj[vertex]:
+            has_cycle = follow_edges(vert, has_cycle)
+        visited[vertex] = False
+
+        return has_cycle
+
+    for vert in range(len(adj)):
+        if has_cycle:
+            break
+        if len(adj[vert]) > 0:
+            has_cycle = follow_edges(adj[vert][0], has_cycle)
+
+    return 1 if has_cycle else 0
+
+
+INTERACTIVE = False
+
+if __name__ == "__main__":
+
+    if INTERACTIVE:
+        input = sys.stdin.read()
+        data = list(map(int, input.split()))
+        adj = create_adjacency_list(data)
+        print(acyclic(adj))
+    else:
+        path, files = read_test_cases.return_path_files(
+            "week2_graph_decomposition2", "1_cs_curriculum"
+        )
+        for file in files:
+            file_path = os.path.join(path, file)
+            adj = parse_input(file_path)
+            res = acyclic(adj)
+            sol = int(open(file_path + ".a").read())
+
+            assert sol == res, f"Error in {file=}"
+            print(f"{file} is DAG: {res == 0}")
